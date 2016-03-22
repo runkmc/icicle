96350b02f16366171c188d4a2ef7506abf8e68e9
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-03-22 14:27:40 -0700

    Initial Commit

---
+++ Icicle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 3/22/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
