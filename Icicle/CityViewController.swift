3048e9bcb17d2331bb3e7030d3e1b5e2a9386305
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-03-31 12:25:50 -0700

    Create CityViewController

---
+++ Icicle/CityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CityViewController.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 3/31/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class CityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
