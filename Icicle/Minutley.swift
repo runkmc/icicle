d901bce2fe4791b6db7d5f518a69c536d59d8134
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-04-06 15:39:13 -0700

    Add Minutely files

---
+++ Icicle/Minutley.swift
@@ -0,0 +1,17 @@
+//
+//  Minutley.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 4/6/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Argo
+import Curry
+
+struct Minutely {
+    let icon: String
+    let summary: String
+    let minutes: [Minute]
+}
