4459e32ae998c96a4957636e8e96944822253baf
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-04-06 15:56:05 -0700

    Create TimeGranularity

---
+++ Icicle/TimeGranularity.swift
@@ -0,0 +1,15 @@
+//
+//  TimeGranularity.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 4/6/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+
+enum TimeGranularity: String {
+    case Day = "daily"
+    case Hour = "hourly"
+    case Minute = "minutely"
+}
