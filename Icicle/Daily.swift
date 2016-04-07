93ed2bde95a621027cb2f9e517290877a008217d
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-04-07 10:49:31 -0700

    Daily parses JSON and builds itself

--- Icicle/Daily.swift
+++ Icicle/Daily.swift
@@ -7,3 +7,20 @@
 //
 
 import Foundation
+import Argo
+import Curry
+import Runes
+
+struct Daily {
+    let icon: String
+    let summary: String
+}
+
+extension Daily: Decodable {
+    static func decode(j:JSON) -> Decoded<Daily> {
+        let f = curry(Daily.init)
+        return f
+            <^> j <| "icon"
+            <*> j <| "summary"
+    }
+}
