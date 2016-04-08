06baf2cba3e4905e0f2f0a879430fbe8dfc008d5
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-04-08 11:03:05 -0700

    Create Hourly view model files

---
+++ Icicle/HourlyViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HourlyViewModel.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 4/8/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
---
+++ IcicleTests/HourlyViewModelSpec.swift
@@ -0,0 +1,9 @@
+//
+//  HourlyViewModelSpec.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 4/8/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
