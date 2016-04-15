736fe50f398270d2e4689b124b998aa7817ad2ec
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-04-14 17:08:24 -0700

    Add boilerplate

--- IcicleTests/HourlyViewModelSpec.swift
+++ IcicleTests/HourlyViewModelSpec.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import Quick
+import Nimble
+import Argo
+@testable import Icicle
+
+class HourlyViewModelSpec: QuickSpec {
+    var bugfix = "" // this is getting old fast.
+    
+    override func spec() {
+        describe("An HourlyViewModelSpec") {
+            
+        }
+    }
+}
