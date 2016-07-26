ab227285e2e9f29005042cb4225d85ab322adf11
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-07-25 23:46:11 -0700

    Remove cocoapods

--- Pods/Target Support Files/Pods-Icicle/Pods-Icicle-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Icicle : NSObject
-@end
-@implementation PodsDummy_Pods_Icicle
-@end
--- Pods/Target Support Files/Pods-IcicleTests/Pods-IcicleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IcicleTests : NSObject
-@end
-@implementation PodsDummy_Pods_IcicleTests
-@end
