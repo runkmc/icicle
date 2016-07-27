39b67c42fd71246de21a4c95dc6c3fa6ccc8f7f5
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-07-27 15:42:58 -0700

    Add JSON type

---
+++ Icicle/JSON.swift
@@ -0,0 +1,9 @@
+//
+//  JSON.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 7/27/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+typealias JSON = [String:AnyObject]
