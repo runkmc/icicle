0966b0f7523b119d1f8c5984c40126f6cb4e4f9e
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-07-27 14:45:09 -0700

    Minute model creates itself from JSON

---
+++ Icicle/Minute.swift
@@ -0,0 +1,27 @@
+//
+//  Minutley.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 4/6/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+
+struct Minute {
+    let precipIntensity: Float
+    let precipProbability: Float
+    let time: Int
+    
+    init?(data:[String:AnyObject]) {
+        if let intensity = data["precipIntensity"] as? Float,
+        probability = data["precipProbability"] as? Float,
+        time = data["time"] as? Int {
+            self.precipIntensity = intensity
+            self.precipProbability = probability
+            self.time = time
+        } else {
+            return nil
+        }
+    }
+}
