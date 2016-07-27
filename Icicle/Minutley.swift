b0fac6e2b799a43936a18155f0987dbbed227bdd
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-07-27 15:07:51 -0700

    Create files for Minutley model

---
+++ Icicle/Minutley.swift
@@ -0,0 +1,9 @@
+//
+//  Minutley.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 7/27/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
---
+++ IcicleTests/MinutleyTest.swift
@@ -0,0 +1,17 @@
+//
+//  MinutleyTest.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 7/27/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class MinutleyTest: XCTestCase {
+    
+    func testMinutleyCreation() {
+        
+    }
+}
