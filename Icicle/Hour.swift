be58b1101d5f379f923eb1fccc663e327af18bdb
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-07-30 19:45:03 -0700

    Add files for Hour model

---
+++ Icicle/Hour.swift
@@ -0,0 +1,9 @@
+//
+//  Hour.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 7/30/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
---
+++ IcicleTests/HourTest.swift
@@ -0,0 +1,9 @@
+//
+//  HourTest.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 7/30/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
