64e1086d621fb9b6c0ef1c905a437a4ad999fedf
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-07-30 20:33:19 -0700

    Add Hour model

--- Icicle/Hour.swift
+++ Icicle/Hour.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+struct Hour {	
+
+	let apparentTemperature:Float
+	let humidity:Float
+	let precipIntensity:Float
+	let precipProbability:Float
+	let summary:String
+	let temperature:Float
+	let time:Int
+	let icon:String
+
+	init?(data:JSON) {
+		if let apparentTemperature = data["apparentTemperature"] as? Float,
+		humidity = data["humidity"] as? Float,
+		precipIntensity = data["precipIntensity"] as? Float,
+		precipProbability = data["precipProbability"] as? Float,
+		summary = data["summary"] as? String,
+		temperature = data["temperature"] as? Float,
+		time = data["time"] as? Int,
+		icon = data["icon"] as? String {
+			self.apparentTemperature = apparentTemperature 
+			self.humidity = humidity
+			self.precipIntensity = precipIntensity
+			self.precipProbability = precipProbability
+			self.summary = summary
+			self.temperature = temperature
+			self.time = time
+			self.icon = icon
+		} else {
+			return nil
+		}
+	}
+}
--- IcicleTests/HourTest.swift
+++ IcicleTests/HourTest.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import XCTest
+@testable import Icicle
+
+class HourTest: XCTestCase {
+
+	func testHourCreation() {
+		let json = ["apparentTemperature":65.5, "humidity":0.55,
+			"precipIntensity":0, "precipProbability":0, "summary":"Partly Cloudy", 
+			"temperature":65.5, "time":1460008800,
+			"icon":"partly-cloudy-night"]
+        let hour = Hour(data:json)!
+        XCTAssertEqual(hour.summary, "partly-cloudy-night")
+	}
+}
