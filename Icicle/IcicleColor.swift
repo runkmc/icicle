a173d28a34d8ef787fa240d4153191bd5ec04106
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-08-12 21:12:16 -0700

    Add struct for custom colors

---
+++ Icicle/IcicleColor.swift
@@ -0,0 +1,17 @@
+//
+//  IcicleColors.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 8/12/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+struct IcicleColor {
+    static let peach = UIColor(red:1.000, green:0.490, blue:0.420, alpha:1.0)
+    static let lightGrey = UIColor(red:0.961, green:0.945, blue:0.886, alpha:1.0)
+    static let blue = UIColor(red:0.604, green:0.753, blue:0.733, alpha:1.0)
+    static let brown = UIColor(red:0.545, green:0.369, blue:0.349, alpha:1.0)
+    static let green = UIColor(red:0.553, green:0.800, blue:0.447, alpha:1.0)
+}
