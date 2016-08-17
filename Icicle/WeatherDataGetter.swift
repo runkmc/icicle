0cc44182c38d1c9f4d0d2a0ca0522283986b0909
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-08-16 17:20:24 -0700

    Create WeatherDataGetter file

---
+++ Icicle/WeatherDataGetter.swift
@@ -0,0 +1,10 @@
+//
+//  WeatherDataGetter.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 8/16/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//  The Author suggests saying WeatherDataGetter five times fast.
+
+import Foundation
+import CoreLocation
