a9a3d22582e59c06cb1987febd30f7e3b0aa602e
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-09-23 23:06:24 -0700

    Add tests for Result

--- IcicleTests/ResultTest.swift
+++ IcicleTests/ResultTest.swift
@@ -9,9 +9,7 @@ class ResultTest: XCTestCase {
         let name: String
     }
     
-    static func flatMapTest(str:String) -> Result<String, [String]> {
-        return .success([str, "and another string"])
-    }
+
     
     func testSuccessAndErrorChecks() {
         let testUser = TestUser(name:"Montgomery Scott")
@@ -22,94 +20,58 @@ class ResultTest: XCTestCase {
         XCTAssertFalse(Result.isSuccess(error))
         XCTAssertTrue(Result.isError(error))
         XCTAssertFalse(Result.isError(success))
-    }            
-//             it("provides lefts and rights") {
-//                 let user1: Either<String, String> = .Right("James Kirk")
-//                 let user2: Either<String, String> = .Right("William Riker")
-//                 let failure: Either<String, String> = .Left("No such user")
-//                 let list: [Either<String, String>] = [user1, user2, failure]
-                
-//                 expect(Either.rights(list)).to(haveCount(2))
-//                 expect(Either.lefts(list)).to(haveCount(1))
-                
-//                 switch (Either.rights(list)[0]) {
-//                 case let (.Right(value)) : expect(value) == "James Kirk"
-//                 default: fatalError()
-//                 }
-                
-//                 switch (Either.rights(list)[1]) {
-//                 case let (.Right(value)) : expect(value) == "William Riker"
-//                 default: fatalError()
-//                 }
-                
-//                 switch (Either.lefts(list)[0]) {
-//                 case let (.Left(value)) : expect(value) == "No such user"
-//                 default: fatalError()
-//                 }
-//             }
-            
-//             it("provides Right values") {
-//                 let data: Either<String, User> = .Right(testUser)
-                
-//                 expect(data.rightValue()!.name) == "Montgomery Scott"
-//             }
-            
-//             it("provides Left values") {
-//                 let data: Either<String, User> = .Left("no such user")
-                
-//                 expect(data.leftValue()!) == "no such user"
-//             }
-            
-//             it("maps Right values") {
-//                 let data: Either<String, String> = .Right("James Kirk")
-//                 let result = data.map { name in User.init(name: name) }
-                
-//                 switch(result) {
-//                 case let .Right(user): expect(user.name) == "James Kirk"
-//                 default: fatalError()
-//                 }
-//             }
-            
-//             it("leaves left values alone during map") {
-//                 let data: Either<String, String> = .Left("a terrible error")
-//                 let result = data.map { n in Int(n)! }
-                
-//                 switch(result) {
-//                 case let .Left(x): expect(x) == "a terrible error"
-//                 default: fatalError()
-//                 }
-//             }
-            
-//             it("flatMaps right values") {
-//                 let e: Either<String, String> = .Right("whatever")
-//                 let result = e.flatMap(EitherSpec.flatMapTest)
-                
-//                 switch result {
-//                 case let .Right(arr): expect(arr) == ["whatever", "and another string"]
-//                 default: fatalError()
-//                 }
-//             }
-            
-//             it("leaves left values alone during flatMap") {
-//                 let e: Either<String, String> = .Left("whatever")
-//                 let result = e.flatMap(EitherSpec.flatMapTest)
-                
-//                 switch result {
-//                 case let .Left(error): expect(error) == "whatever"
-//                 default: fatalError()
-//                 }
-//             }
-            
-//             it("uses >>- for flatMap") {
-//                 let e: Either<String, String> = .Right("whatever")
-//                 let result = e >>- EitherSpec.flatMapTest
-                
-//                 switch result {
-//                 case let .Right(arr): expect(arr) == ["whatever", "and another string"]
-//                 default: fatalError()
-//                 }
-//             }
-//         }
-//     }
+    }
+    
+    func testFiltering() {
+        let tu1: Result<String, TestUser> = .success(TestUser(name: "James Kirk"))
+        let tu2: Result<String, TestUser> = .success(TestUser(name: "Jean-Luc Picard"))
+        let fail: Result<String, TestUser> = .error("no user")
+        let list = [tu1, tu2, fail]
+        
+        XCTAssertEqual(Result.errors(list).count, 1)
+        XCTAssertEqual(Result.successes(list).count, 2)
+    }
+    
+    func testGettingValues() {
+        let tu1: Result<String, TestUser> = .success(TestUser(name: "James Kirk"))
+        let fail: Result<String, TestUser> = .error("no user")
+        
+        XCTAssertEqual(tu1.successValue()!.name, "James Kirk")
+        XCTAssertEqual(fail.errorValue()!, "no user")
+    }
+    
+    func testMap() {
+        let tu1: Result<String, String> = .success("James Kirk")
+        let result: Result<String, TestUser> = tu1.map { name in TestUser(name: name) }
+        XCTAssertEqual(result.successValue()!.name, "James Kirk")
+        
+        let fail: Result<String, String> = .error("no user")
+        let failResult: Result<String, TestUser> = fail.map { name in TestUser(name: name) }
+        XCTAssertEqual(failResult.errorValue()!, "no user")
+    }
+    
+    func testFlatMap() {
+        func flatMapTest(str:String) -> Result<String, [String]> {
+            return .success([str, "and another string"])
+        }
+        
+        let s: Result<String, String> = .success("yes!")
+        let successResult = s.flatMap(flatMapTest)
+        XCTAssertEqual(successResult.successValue()!, ["yes!", "and another string"])
+        
+        let f: Result<String, String> = .error("no!")
+        let failResult = f.flatMap(flatMapTest)
+        XCTAssertEqual(failResult.errorValue()!, "no!")
+    }
+
+    func testFlatMapOperator() {
+        func flatMapTest(str:String) -> Result<String, [String]> {
+            return .success([str, "and another string"])
+        }
+    
+        let s: Result<String, String> = .success("yes!")
+        let successResult = s >>- flatMapTest
+        XCTAssertEqual(successResult.successValue()!, ["yes!", "and another string"])
+    }
 
 }
