9584ed8bae4f5c90c9c961b24747edd9dda8c5ad
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-09-25 13:32:40 -0700

    Setup for Argo

--- Icicle/Minute.swift
+++ Icicle/Minute.swift
@@ -1,15 +1,18 @@
 //
-//  Minutley.swift
+//  Minute.swift
 //  Icicle
 //
-//  Created by Kevin McGladdery on 4/6/16.
+//  Created by Kevin McGladdery on 9/25/16.
 //  Copyright © 2016 Kevin McGladdery. All rights reserved.
 //
 
 import Foundation
+import Argo
+import Runes
+import Curry
 
 struct Minute {
-    let precipIntensity: Float?
-    let precipProbability: Float?
+    let precipIntensity: Float
+    let precipProbability: Float
     let time: Double
 }
