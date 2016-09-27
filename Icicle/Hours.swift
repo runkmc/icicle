0621bdd7ed21af2386d9e96186a6e7faf06eb0f8
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-09-27 11:17:56 -0700

    Update models to use Argo

---
+++ Icicle/Hours.swift
@@ -0,0 +1,27 @@
+//
+//  Hours.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 9/27/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Argo
+import Runes
+import Curry
+
+struct Hours {
+    let summary:String?
+    let icon:String?
+    let hours:[Hour]?
+}
+
+extension Hours: Decodable {
+    static func decode(_ json: JSON) -> Decoded<Hours> {
+        return curry(Hours.init)
+        <^> json <|? "summary"
+        <*> json <|? "icon"
+        <*> json <||? "data"
+    }
+}
---
+++ IcicleTests/HoursTest.swift
@@ -0,0 +1,25 @@
+//
+//  HoursTest.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 9/27/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Argo
+import XCTest
+@testable import Icicle
+
+class HoursTest: XCTestCase {
+    func testCreation() {
+        let data = getTestJSON(named: "cloudy-futurerain", forClass: type(of:self))
+        let json = parseJSON(data: data, granularity: .hour)
+        let hours: Decoded<Hours> = decode(json.successValue()!)
+        let h = hours.value!
+        
+        XCTAssertEqual(h.summary!, "Partly cloudy until this evening.")
+        XCTAssertEqual(h.icon!, "partly-cloudy-day")
+        XCTAssertEqualWithAccuracy(h.hours![0].temperature!, 60.84, accuracy: 0.01)
+    }
+}
