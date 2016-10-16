41dbee59df372512a52284db62576c0c49444fb3
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-10-15 18:36:29 -0700

    Add Currently model

---
+++ Icicle/Currently.swift
@@ -0,0 +1,33 @@
+//
+//  Currently.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 10/8/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Argo
+import Runes
+import Curry
+
+struct Currently {
+    let time:Double
+    let summary:String?
+    let temperature:Float?
+    let apparentTemperature:Float?
+    let precipProbability:Float?
+    let precipType:String?
+}
+
+extension Currently: Decodable {
+    static func decode(_ json: JSON) -> Decoded<Currently> {
+        return curry(Currently.init)
+        <^> json <| "time"
+        <*> json <|? "summary"
+        <*> json <|? "temperature"
+        <*> json <|? "apparentTemperature"
+        <*> json <|? "precipProbability"
+        <*> json <|? "precipType"
+    }
+}
---
+++ IcicleTests/CurrentlyTest.swift
@@ -0,0 +1,28 @@
+//
+//  CurrentlyTest.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 10/8/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import Argo
+import XCTest
+@testable import Icicle
+
+class CurrentlyTest: XCTestCase {
+    func testCreation() {
+        let data = getTestJSON(named: "sunny-hot", forClass: type(of: self))
+        let json = parseJSON(data: data, granularity: .current)
+        let currently: Decoded<Currently> = decode(json.successValue()!)
+        let c = currently.value!
+        
+        XCTAssertEqual(c.time, 1474830537)
+        XCTAssertEqual(c.summary!, "Clear")
+        XCTAssertEqualWithAccuracy(c.temperature!, 83.12, accuracy: 0.01)
+        XCTAssertEqualWithAccuracy(c.apparentTemperature!, 81.45, accuracy: 0.01)
+        XCTAssertEqualWithAccuracy(c.precipProbability!, 0, accuracy: 0.001)
+        XCTAssertNil(c.precipType)
+    }
+}
