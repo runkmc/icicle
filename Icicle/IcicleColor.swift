602bf2c3f7331eb3d6f7f010db4028ab8106e9a1
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-10-18 17:03:45 -0700

    Remove old colors

--- Icicle/IcicleColor.swift
+++ Icicle/IcicleColor.swift
@@ -9,9 +9,4 @@
 import UIKit
 
 struct IcicleColor {
-    static let peach = UIColor(red:1.000, green:0.490, blue:0.420, alpha:1.0)
-    static let lightGrey = UIColor(red:0.961, green:0.945, blue:0.886, alpha:1.0)
-    static let blue = UIColor(red:0.604, green:0.753, blue:0.733, alpha:1.0)
-    static let brown = UIColor(red:0.545, green:0.369, blue:0.349, alpha:1.0)
-    static let green = UIColor(red:0.553, green:0.800, blue:0.447, alpha:1.0)
 }
