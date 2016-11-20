e4eb5d548f4c5f05f30b8925965bb1edd99bc141
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-11-20 14:55:44 -0800

    Create Day viewmodel files

---
+++ Icicle/DayData.swift
@@ -0,0 +1,13 @@
+//
+//  DayData.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 11/20/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+
+struct DayData {
+    
+}
---
+++ IcicleTests/DayDataTest.swift
@@ -0,0 +1,9 @@
+//
+//  DayDataTest.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 11/20/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
