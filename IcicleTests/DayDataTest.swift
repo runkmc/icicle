6478686a33bd0da753656bc0c1637430b9ce91d3
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-11-26 22:03:47 -0800

    Added a second set of test cases

--- IcicleTests/DayDataTest.swift
+++ IcicleTests/DayDataTest.swift
@@ -12,7 +12,7 @@ import Argo
 @testable import Icicle
 
 class DayDataTest: XCTestCase {
-    func testCreation() {
+    func testSunnyCreation() {
         let data = getTestJSON(named: "sunny-hot", forClass: type(of:self))
         let json = parseJSON(data: data, granularity: .day)
         let dayJSON = json.successValue()!
@@ -26,6 +26,24 @@ class DayDataTest: XCTestCase {
         XCTAssertEqual("56° at 6AM", day.low)
         XCTAssertEqual("7:00 AM", day.sunrise)
         XCTAssertEqual("7:01 PM", day.sunset)
+        XCTAssertEqual("None", day.precipChance)
     }
     
+    func testRainyCreation() {
+        let data = getTestJSON(named: "cloudy-futurerain", forClass: type(of:self))
+        let json = parseJSON(data: data, granularity: .day)
+        let dayJSON = json.successValue()!
+        let days = dayJSON["data"] as! [AnyObject]
+        let dayInfo: Decoded<Day> = decode(days[4] as! [String:Any])
+        let timeZone = parseTimeZone(data: data).successValue()!
+        let day = DayData.create(dayInfo, timeZone:timeZone).successValue()!
+        
+        XCTAssertEqual("9/29", day.date)
+        XCTAssertEqual("65° at 4PM", day.high)
+        XCTAssertEqual("56° at 11PM", day.low)
+        XCTAssertEqual("6:37 AM", day.sunrise)
+        XCTAssertEqual("6:26 PM", day.sunset)
+        XCTAssertEqual("55%", day.precipChance)
+
+    }
 }
