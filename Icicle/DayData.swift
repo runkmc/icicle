6e5acf7db75b42a3d215dd897051636cf272d02c
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-11-26 21:37:00 -0800

    Add high, low, sunset, and sunrise to DayData

--- Icicle/DayData.swift
+++ Icicle/DayData.swift
@@ -10,20 +10,59 @@ import Foundation
 import Argo
 
 struct DayData {
-    static let formatter: DateFormatter = {
+    let date: String
+    let high: String
+    let low: String
+    let sunrise: String
+    let sunset: String
+    
+    static let dateformatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.locale = Locale.current
         formatter.dateFormat = "M/d"
         return formatter
     }()
     
-    let date: String
+    static let timeformatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale.current
+        formatter.dateFormat = "ha"
+        return formatter
+    }()
     
-    static func create(decodedDay:Decoded<Day>) -> Result<[String], DayData> {
+    static let specificTimeformatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale.current
+        formatter.dateFormat = "h:mm a"
+        return formatter
+    }()
+    
+    static func create(_ decodedDay:Decoded<Day>, timeZone:TimeZone) -> Result<[String], DayData> {
         guard let day = decodedDay.value else {
             return .error(["oops"])
         }
-        let date = self.formatter.string(from: Date(timeIntervalSince1970: day.time))
-        return .success(DayData(date: date))
+        dateformatter.timeZone = timeZone
+        
+        let date = self.dateformatter.string(from: Date(timeIntervalSince1970: day.time))
+        let high = parseTempString(temp: day.temperatures.temperatureMax, time: day.temperatures.temperatureMaxTime)
+        let low = parseTempString(temp: day.temperatures.temperatureMin, time: day.temperatures.temperatureMinTime)
+        var sunrise = "Unknown"
+        if let sunriseTime = day.sunriseTime {
+            sunrise = specificTimeformatter.string(from: Date(timeIntervalSince1970: sunriseTime))
+        }
+        var sunset = "Unknown"
+        if let sunsetTime = day.sunsetTime {
+            sunset = specificTimeformatter.string(from: Date(timeIntervalSince1970: sunsetTime))
+        }
+        
+        return .success(DayData(date: date, high: high, low: low, sunrise: sunrise, sunset: sunset))
+    }
+    
+    static func parseTempString(temp:Float, time:Double?) -> String {
+        var timeString = "\(Int(temp))°"
+        if let timeDouble = time {
+            timeString.append(" at \(timeformatter.string(from: Date(timeIntervalSince1970: timeDouble)))")
+        }
+        return timeString
     }
 }
--- IcicleTests/DayDataTest.swift
+++ IcicleTests/DayDataTest.swift
@@ -18,9 +18,14 @@ class DayDataTest: XCTestCase {
         let dayJSON = json.successValue()!
         let days = dayJSON["data"] as! [AnyObject]
         let dayInfo: Decoded<Day> = decode(days[0] as! [String:Any])
-        let day = DayData.create(decodedDay:dayInfo).successValue()!
+        let timeZone = parseTimeZone(data: data).successValue()!
+        let day = DayData.create(dayInfo, timeZone:timeZone).successValue()!
 
         XCTAssertEqual("9/25", day.date)
+        XCTAssertEqual("94° at 4PM", day.high)
+        XCTAssertEqual("56° at 6AM", day.low)
+        XCTAssertEqual("7:00 AM", day.sunrise)
+        XCTAssertEqual("7:01 PM", day.sunset)
     }
     
 }
