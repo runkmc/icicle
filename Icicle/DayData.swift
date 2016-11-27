434dd79a1d2458e1d88a3a8b0752e2ef9d5d74da
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-11-26 22:03:38 -0800

    Added precipChance

--- Icicle/DayData.swift
+++ Icicle/DayData.swift
@@ -15,6 +15,7 @@ struct DayData {
     let low: String
     let sunrise: String
     let sunset: String
+    let precipChance: String
     
     static let dateformatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -42,15 +43,22 @@ struct DayData {
             return .error(["oops"])
         }
         dateformatter.timeZone = timeZone
+        timeformatter.timeZone = timeZone
+        specificTimeformatter.timeZone = timeZone
         
         let date = self.dateformatter.string(from: Date(timeIntervalSince1970: day.time))
         let high = parseTempString(temp: day.temperatures.temperatureMax, time: day.temperatures.temperatureMaxTime)
         let low = parseTempString(temp: day.temperatures.temperatureMin, time: day.temperatures.temperatureMinTime)
         let sunrise = day.sunriseTime != nil ? specificTimeformatter.string(from: Date(timeIntervalSince1970: day.sunriseTime!)) : "Unknown"
         let sunset = day.sunsetTime != nil ? specificTimeformatter.string(from: Date(timeIntervalSince1970: day.sunsetTime!)) : "Unknown"
-        
+        let precipChance: String
+        switch day.precipProbability {
+        case .none: precipChance = "None"
+        case .some(let value) where value == 0: precipChance = "None"
+        case .some(let value): precipChance = String(format: "%.0f%%", (value * 100))
+        }
 
-        return .success(DayData(date: date, high: high, low: low, sunrise: sunrise, sunset: sunset))
+        return .success(DayData(date: date, high: high, low: low, sunrise: sunrise, sunset: sunset, precipChance: precipChance))
     }
     
     static func parseTempString(temp:Float, time:Double?) -> String {
