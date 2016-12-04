3b55c5f6df69d899c647459b5fcad7fe74b793d3
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-12-03 16:24:49 -0800

    Add test data with precipitation

--- IcicleTests/HourDataTest.swift
+++ IcicleTests/HourDataTest.swift
@@ -23,4 +23,17 @@ class HourDataTest: XCTestCase {
         
         XCTAssertEqual("12 PM", hour.time)
     }
+    
+    func testRainyCreation() {
+        let data = getTestJSON(named: "cloudy-futurerain", forClass: type(of:self))
+        let json = parseJSON(data: data, granularity: .hour)
+        let hourJSON = json.successValue()!
+        let hours = hourJSON["data"] as! [AnyObject]
+        let hourInfo: Decoded<Hour> = decode(hours[44] as! [String:Any])
+        let timeZone = parseTimeZone(data: data).successValue()!
+        let hour = HourData.create(hourInfo, timeZone:timeZone).successValue()!
+        
+        XCTAssertEqual("11 AM", hour.time)
+    }
+
 }
