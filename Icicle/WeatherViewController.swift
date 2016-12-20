4462a409b35d4b0895998fc62ed10b487fbb5fb1
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2016-12-19 19:59:12 -0800

    Add UIScrollView to controller

--- Icicle/WeatherViewController.swift
+++ Icicle/WeatherViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
