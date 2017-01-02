0808de986fa0e64f2a7ff2063f1ae7a0de26ad87
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-01-01 19:46:02 -0800

    Add LocationService

---
+++ Icicle/LocationService.swift
@@ -0,0 +1,39 @@
+//
+//  LocationService.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 12/23/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import RxCocoa
+import RxSwift
+
+class LocationService: NSObject {
+    static let instance = LocationService()
+    fileprivate let manager = CLLocationManager()
+    var currentLocation: CLLocation? = nil
+    
+    fileprivate override init() { }
+    
+    fileprivate func startGettingLocation(status:CLAuthorizationStatus = CLLocationManager.authorizationStatus()) {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            manager.requestLocation()
+        default:
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.startGettingLocation(status: status)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+    }
+}
---
+++ IcicleTests/LocationServiceTest.swift
@@ -0,0 +1,9 @@
+//
+//  LocationServiceTest.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 12/23/16.
+//  Copyright © 2016 Kevin McGladdery. All rights reserved.
+//
+
+import Foundation
