2150313068f4635ea846b71c222d7ea8cfeede45
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-01-09 13:38:12 -0800

    Add some test views

--- Icicle/WeatherViewController.swift
+++ Icicle/WeatherViewController.swift
@@ -11,27 +11,27 @@ import UIKit
 class WeatherViewController: UIViewController {
 
     @IBOutlet weak var scrollView: UIScrollView!
+    var locationService: LocationService? = nil
+    @IBOutlet weak var headerBackground: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let locService = CurrentLocationService.instance
+        print("got to view will appear")
+        weatherFetcher(locationService: locService, session: URLSession.shared, key: forecastAPIKEY) { [weak self] result in
+            DispatchQueue.main.sync {
+                print("block ran")
+            }
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
