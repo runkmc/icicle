8a4fd0e519a0e7456bc53e01955af835e12b2fb6
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-01-14 15:03:14 -0800

    Add DailyCollectionViewCell

---
+++ Icicle/DailyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DailyCollectionViewCell.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 1/14/17.
+//  Copyright © 2017 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class DailyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
