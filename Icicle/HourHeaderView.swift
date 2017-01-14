668953521b71eecaeccd5e2da691adb47eba2772
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-01-13 16:24:07 -0800

    Add HourHeaderView

---
+++ Icicle/HourHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  HourHeaderView.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 1/13/17.
+//  Copyright © 2017 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+class HourHeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
