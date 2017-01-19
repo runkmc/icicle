14ceabd7f95e47ec0ddb418ccde73b1e393f0e28
Kevin McGladdery <kevin.mcgladdery@gmail.com>
2017-01-19 09:10:06 -0800

    Add file for gradient helper

---
+++ Icicle/gradient.swift
@@ -0,0 +1,18 @@
+//
+//  gradient.swift
+//  Icicle
+//
+//  Created by Kevin McGladdery on 1/19/17.
+//  Copyright © 2017 Kevin McGladdery. All rights reserved.
+//
+
+import UIKit
+
+//func gradientFor(weather: WeatherIcon, view: UIView) -> CAGradientLayer {
+//    var color1: UIColor
+//    var color2: UIColor
+//    switch weather {
+//        
+//    }
+//    
+//}
